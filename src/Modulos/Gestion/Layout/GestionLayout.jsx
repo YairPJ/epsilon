13aa140822c743b54ea97e6ad6047fdb5a74f78c
yair puga <yair-67@hotmail.com>
2023-08-08 16:56:02 -0600

    Nuevos cambios

---
+++ src/Modulos/Gestion/Layout/GestionLayout.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import { HomeLayout } from '../../../Home/Layout/HomeLayout'
+
+export const GestionLayout = () => {
+  return (
+    <HomeLayout>
+        
+    </HomeLayout>
+  )
+}
---
+++ src/Modulos/Gestion/Pages/Gestion.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { GestionLayout } from '../Layout/GestionLayout'
+
+export const Gestion = () => {
+  return (
+    <GestionLayout>
+        
+    </GestionLayout>
+  )
+}
